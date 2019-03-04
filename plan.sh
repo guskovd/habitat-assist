b2e9c5ca1f7be68e34d6749dc310054bf3ce7f30
Danil Guskov <d.guskov@corp.mail.ru>
2019-03-04 14:56:56 +0300

    plan sh lock

---
+++ plan.sh
@@ -0,0 +1,46 @@
+pkg_name=habitat-assist
+pkg_origin=guskovd
+pkg_version='1.0.0'
+pkg_description="Habitat assist"
+pkg_maintainer='Guskovd'
+pkg_upstream_url="https://github.com/guskovd/habitat-assist"
+
+pkg_hab_shell_interpreter="bash"
+
+RUBY_VERSION=2.5.1
+
+pkg_deps=(
+    core/sudo
+    core/hab
+    core/bash
+    core/gawk
+    core/grep
+    core/git
+    core/gcc
+    core/gcc-libs
+    core/make
+    core/clang
+    core/openssl
+    core/pkg-config
+    core/libsodium
+    core/libarchive
+    core/ruby/$RUBY_VERSION
+    core/docker
+    core/rsync
+    core/sshpass
+    guskovd/rust-nightly
+    guskovd/rust-racer
+)
+
+do_shell() {
+    . ~/.bashrc
+}
+
+do_build() {
+    return 0
+}
+
+do_install() {
+    return 0
+}
+
