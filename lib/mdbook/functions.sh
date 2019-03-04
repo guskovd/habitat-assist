0355cdb5a4c253e9d64d982625ec0d276bd9821f
Danil Guskov <d.guskov@corp.mail.ru>
2019-03-04 15:24:18 +0300

    mdbook

--- lib/mdbook/functions.sh
+++ lib/mdbook/functions.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env sh
 # -*- coding: utf-8 -*-
 
+BOOK_PATH='_book'
+
 function mdbook::build() {
-    echo "building"
+    echo "building ..."
+    mkdir -p ${BOOK_PATH}/src
+    rsync -r --include='*/' --include='*.md' --exclude='*' $PWD/ ./${BOOK_PATH}/src
 }
